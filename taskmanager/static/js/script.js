888d6435e716b512afb797b817dd1cf778c1fd45
bojaymes <seyibaba02@aol.com>
2024-05-23 13:04:01 +0000

    add add_task html, add js cript for new task functionality, date picker, urgent button selection and textarea adjustiment, update routes.py, category.html, tasks.html and base.html with the add_task functions

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,10 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // sidenav initialization
+document.addEventListener("DOMContentLoaded", function () {
+    // Sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
+    // Modal initialization
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+});
