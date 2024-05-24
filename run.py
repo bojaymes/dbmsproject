e0e966d8c360ddce9c105264d52d8c2e03d464d1
bojaymes <seyibaba02@aol.com>
2024-05-24 11:32:05 +0000

    update run.py file

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from taskmanager import app
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
